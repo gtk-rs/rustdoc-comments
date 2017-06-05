3e7f789f85d02039a42615bf32036956ab20dcd6
Guillaume Gomez <guillaume1.gomez@gmail.com>
2017-06-05 20:02:20 +0200

    Add sourceview doc

---
+++ implementors/pango/matrix/trait.Matrix.js
@@ -0,0 +1,13 @@
+(function() {var implementors = {};
+implementors["gdk"] = [];
+implementors["gtk"] = [];
+implementors["pango"] = [];
+implementors["sourceview"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
